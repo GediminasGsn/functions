6f0368ab64b671dfb3641fd9a10ed789bd336cee
GediminasGsn <gasiunasgediminas@gmail.com>
2024-03-19 18:52:02 +0200

    ***

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main(String[] args) {
         int n1 = 6;
         int n2 = 6;
-        sudetis(n1, n2);
-        // System.out.println(9.8596);
-        System.out.println(PISq());
-        double res = PISq();
-        System.out.println(res);
+        int sumResult = sudetis(n1, n2);
+        System.out.println("Sum: " + sumResult);
+
+        double piSquared = PISq();
+        System.out.println("Pi squared: " + piSquared);
 
         int nr1 = 2;
         int nr2 = 2;
-        sandauga(nr1, nr2);
+        int productResult = sandauga(nr1, nr2);
+        System.out.println("Product: " + productResult);
 
         int[] arr = {6, 7, 8, 9, 6, 3, 2, 4};
         printArr(arr);
@@ -18,15 +19,16 @@ public class Main {
         int min = 10;
         int max = 50;
         int randomNum = getRandom(min, max);
+        System.out.println("Skaicius: " + randomNum);
 
+        int[] randArr = randArr (5, 9, 10)
 
 
     }
 
     public static int sudetis(int int1, int int2) {
-        int sudetis = int1 + int2;
-        System.out.println(sudetis);
-        return sudetis;
+        int sum = int1 + int2;
+        return sum;
     }
 
     public static double PISq() {
@@ -34,9 +36,8 @@ public class Main {
     }
 
     public static int sandauga(int nr1, int nr2) {
-        int sandauga = nr1 * nr2;
-        System.out.println(sandauga);
-        return sandauga;
+        int product = nr1 * nr2;
+        return product;
     }
 
     public static void printArr(int[] arr) {
@@ -45,9 +46,17 @@ public class Main {
         }
         System.out.println();
     }
-    public static int getRandom(int min, int max){
-        return min + (int) (Math.random() * (max - min + 1));
 
+    public static int getRandom(int min, int max) {
+        return min + (int) (Math.random() * (max - min + 1));
     }
+//    Sukurkite Funkciją kuri sugeneruotų random int skaičių masyvą ir jį gražintų. Funkcija priima tris int tipo kintamuosius, min, max ir length
+    public static int sestas(int min, int max, int lenght){
+        for (int i = 0; i < lenght; i++) {
+            System.out.println(arr[i] + " ");
+
+        }
+    }
+
 
 }
